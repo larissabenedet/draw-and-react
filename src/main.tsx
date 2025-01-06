29adb939c67738ebde14c8f5ccc42cd59dfb4bf2
laribenedet <larissavbenedet@gmail.com>
2025-01-05 21:39:43 -0300

    chore: move globalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import App from './App'
 import { StrictMode } from 'react'
-import GlobalStyle from './GlobalStyles'
+import GlobalStyle from './themes/GlobalStyles'
 import { createRoot } from 'react-dom/client'
 import { ModelProvider } from './contexts/ModelContext'
 import { WebcamProvider } from './contexts/WebcamContext'
