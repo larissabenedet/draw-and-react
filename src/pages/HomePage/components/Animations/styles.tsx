7861ac12c7f7dfe47029979ae96aab15bc1da8d7
laribenedet <larissavbenedet@gmail.com>
2025-01-11 17:51:44 -0300

    style: fix error typo

--- src/pages/HomePage/components/Animations/styles.tsx
+++ src/pages/HomePage/components/Animations/styles.tsx
@@ -4,8 +4,8 @@ export const MainDiv = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: '100%';
-  height: '100%';
+  width: 100%;
+  height: 100%;
   pointer-events: 'none';
   z-index: 1000;
 `
